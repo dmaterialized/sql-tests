c624ec9b449c084d4af28e3a787623485dfe536e
D.M. Cook <dmaterialized@gmail.com>
2017-05-08 00:34:07 -0400

    test add and delete funcs

--- script1.py
+++ script1.py
@@ -27,10 +27,10 @@ def create_table():
     cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
     # sql code goes in the brackets, always in quotes.
     # Pass code as a string to Execute method of cursor.
-    # The above creates a table named store with four columns(?)
+    # The above creates a table named store with four columns.
     # the if not exists is to ensure that it can handle being already present.
     conn.commit() # save those changes to the database
-    conn.close()
+    conn.close() # close connection
 
 def insert(item,quantity,price): # ensure that you set the arguments
     conn=sqlite3.connect("lite.db")
@@ -45,11 +45,11 @@ def insert(item,quantity,price): # ensure that you set the arguments
     # first list the ???, then, after the SQL code, identify variables
     cur.execute("INSERT INTO store VALUES (?,?,?)",(item,quantity,price))
     conn.commit() # save those changes to the database
-    conn.close()
+    conn.close() # close connection
 
 # ==== b e g i n   i n s e r t i o n  =========================
 
-insert("Water glass",10,5)
+# insert("Water glass",10,5)
 # will keep adding this item no matter what - not so good.
 
 # ==== s e t   u p   v i e w s  =========================
@@ -65,4 +65,21 @@ def view():
 
 # need a way to update/delete.
 
+# ==== s e t   u p   d a t a   f u n c t i o n s  =========================
+
+def delete(item):
+    conn=sqlite3.connect("lite.db")
+    cur=conn.cursor()
+    cur.execute("DELETE FROM store WHERE item=?", (item,))
+    # that ending comma is very important!!
+    # does a straight text search, removes ALL instances.
+    conn.commit()
+    conn.close()
+
+def update(item):
+    conn=sqlite3.connect("lite.db")
+    cur=conn.cursor()
+    cur.execute("UPDATE store SET quantity=?, price=?" )
+
+# delete("Water glass")  # removed every instance.
 print(view())
